d645f1b2bb2045343524b3855e3558bf3dbcff11
Igor Zinovich <diego.sablezub97@gmail.com>
2017-10-29 10:18:41 +0300

    Linux add

--- Laba1/ClientLaba1.cpp
+++ Laba1/ClientLaba1.cpp
@@ -1,58 +1,163 @@
-#include <winsock2.h>
+
 #include <iostream>
 #include <stdlib.h>
 
+
+#ifdef linux
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netinet/in.h>  //
+#include <netdb.h>  //
+#include <memory.h> //
+typedef int mySocket;
+#endif
+
 #define BUFFER 1024 
 
-#pragma comment(lib,"Wsock32.lib")
+#ifdef WIN32
+#include <winsock2.h>
+#include <Mstcpip.h>
+#pragma		comment (lib,"ws2_32.lib")
+typedef SOCKET mySocket;
+#endif
+
 using namespace std;
 
-SOCKET initial();
-void workClient(SOCKET);
+mySocket initial();
+void clear();
+void workClient(mySocket);
 char** parcing(char*);
-void upload(SOCKET, char*);
-void getRes(SOCKET);
+void upload(mySocket, char*);
+int getRes(mySocket);
+void closeMySocket(mySocket);
 
 int main()
 {
-	
-	SOCKET s = initial();
-	workClient(s);
-	
-	closesocket(s);
-	WSACleanup();
+
+	mySocket s = initial();
+	if (s != -1)
+	{
+		workClient(s);
+		closeMySocket(s);
+	}
+	else
+	{
+		cout << "application closed\npress ane key" << endl;
+		getchar();
+	}
+	clear();
 	return 0;
 
 }
 
-SOCKET initial()
+void closeMySocket(mySocket s)
 {
+#ifdef WIN32
+	closesocket(s);
+#endif
+
+#ifdef linux
+	close(s);
+#endif
+}
+
+void clear()
+{
+#ifdef WIN32
+	WSACleanup();
+#endif
+}
+
+mySocket initial()
+{
+#ifdef WIN32
 	WORD wVersionRequested;
 	WSADATA wsaData;
 	wVersionRequested = MAKEWORD(2, 2);
 	WSAStartup(wVersionRequested, &wsaData);
+#endif
 	struct sockaddr_in peer;
 	peer.sin_family = AF_INET;
 	peer.sin_port = htons(1280);
 	peer.sin_addr.s_addr = inet_addr("127.0.0.1");
-	SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
-	connect(s, (struct sockaddr*) &peer, sizeof(peer));
+	mySocket s = socket(AF_INET, SOCK_STREAM, 0);
+	if (s<0)
+	{
+		cout << "Socket Error" << endl;
+		return -1;
+	}
+#ifdef linux
+	
+	int keepalive = 1;
+	if (setsockopt(s, SOL_SOCKET, SO_KEEPALIVE, &keepalive, (socklen_t)sizeof(keepalive)) < 0)
+	{
 
-	return s;
+		perror("setsockopt(keepalive)");
+	}
+	int keepcnt = 1;
+	if (setsockopt(s, IPPROTO_TCP, TCP_KEEPCNT, &keepcnt, (socklen_t)sizeof(int))<0)
+	{
+
+		perror("setsockopt(keepcnt)");
+	}
+	int keepidle = 30;
+	if (setsockopt(s, IPPROTO_TCP, TCP_KEEPIDLE, &keepidle, (socklen_t)sizeof(int))<0)
+	{
+
+		perror("setsockopt(keepidle)");
+	}
+	int keepintvl = 1;
+	if (setsockopt(s, IPPROTO_TCP, TCP_KEEPINTVL, &keepintvl, (socklen_t)sizeof(int))<0)
+	{
+		perror("setsockopt(keepalive)");
+	}
+#endif
+#ifdef WIN32
+	tcp_keepalive alive;
+	alive.onoff = 1;
+	alive.keepaliveinterval = 3;
+	alive.keepalivetime = 30000;
+	DWORD ret;
+
+	if (WSAIoctl(s, SIO_KEEPALIVE_VALS, &alive, sizeof(alive), NULL, 0, &ret, NULL, NULL) == SOCKET_ERROR)
+	{
+		cout << "ERROR KEEPALIVE" << endl;
+	}
+#endif
+
+	int con_res = connect(s, (struct sockaddr*) &peer, sizeof(peer));
+	if (!(con_res < 0))
+	{
+		return s;
+	}
+	cout << "connect error" << endl;
+	closeMySocket(s);
+	return -1;
 }
 
-void workClient(SOCKET s)
+void workClient(mySocket s)
 {
 	bool flag = true;
 	do
 	{
 		char cmd[128];
-		gets(cmd);
-		send(s, cmd, sizeof(cmd), 0);
+		cout << "> ";
+		fgets(cmd, 128, stdin);
+		FILE* file = NULL;
 		char** cmdp = parcing(cmd);
+		if (send(s, cmd, sizeof(cmd), 0)<0)
+		{
+			flag = false;
+			cout << "problem with connection" << endl;
+		}
 		switch (cmdp[0][0])
 		{
-		case 'u': upload(s, cmdp[1]); break;
+		case 'u': upload(s, cmdp[1]);
+			break;
 		case 'c': flag = false; break;
 		case 't':
 		case 'e': getRes(s); break;
@@ -61,19 +166,55 @@ void workClient(SOCKET s)
 
 }
 
-void upload(SOCKET s, char *path)
+void upload(mySocket s, char *path)
 {
 	char* outbuffer = new char[BUFFER + 1];
-	byte *b = new byte[BUFFER];
-
+	char *b = new char[BUFFER];
 	FILE* file = fopen(path, "rb");
-
-	while (1)
+	int sizeFile = -1;
+	if (!file)
 	{
-		int size = fread(b, sizeof(byte), BUFFER, file);
-		send(s, (char*)b, size, 0);
-		if (size != BUFFER) break;
+		cout << "can't open file" << endl;
+		if (send(s, (char*)(&sizeFile), sizeof(sizeFile), 0)<0)
+		{
+			fclose(file);
+			cout << "Error sending data" << endl;
+			return;
+		}
+		return;
 	}
+
+	fseek(file, 0, SEEK_END);
+	sizeFile = ftell(file);
+	if (send(s, (char*)(&sizeFile), sizeof(sizeFile), 0)<0)
+	{
+		fclose(file);
+		cout << "Error sending data" << endl;
+		return;
+	}
+	fseek(file, 0, SEEK_SET);
+	cout << "upload is starting" << endl;
+	int sizeUpload = 0;
+	while (sizeUpload < sizeFile)
+	{
+		int size = fread(b, sizeof(char), BUFFER, file);
+
+		if (send(s, b, size, 0)<0)
+		{
+			cout << "Error sending data" << endl;
+			fclose(file);
+			return;
+		}
+		sizeUpload += size;
+		int per, per1 = -1;
+		per = (int)((double)sizeUpload / (double)sizeFile * 100);
+		if (per != per1)
+		{
+			cout << "\r" << per << "%";
+			per1 = per;
+		}
+	}
+	cout << "\nupload finished" << endl;
 	fclose(file);
 }
 
@@ -81,14 +222,13 @@ char** parcing(char* str)
 {
 	char** response;
 	response = new char*[3];
-
 	int  i = 0;
 	int count = 0;
 	do
 	{
 		char* word = new char[BUFFER];
 		int j = 0;
-		while (str[i] != ' ' && str[i] != '\0')
+		while (str[i] != ' ' && str[i] != '\0' && str[i] != '\n')
 		{
 			word[j] = str[i];
 			i++;
@@ -103,9 +243,14 @@ char** parcing(char* str)
 	return response;
 }
 
-void getRes(SOCKET s)
+int getRes(mySocket s)
 {
 	char buffer[128];
-	recv(s, buffer, sizeof(buffer), 0);
-	cout << buffer << endl;
+	if (recv(s, buffer, sizeof(buffer), 0)<0)
+	{
+		cout << "Error retrieving data" << endl;
+		return 0;
+	}
+	cout << "response from server: " << buffer << endl;
+	return 1;
 }
--- Laba1/ServerLaba1.cpp
+++ Laba1/ServerLaba1.cpp
@@ -1,47 +1,100 @@
-#include <winsock2.h>
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 #define BUFFER 1024 
-#pragma comment(lib,"Wsock32.lib")
+#define STR "File ended"
+
+#ifdef linux
+#include <unistd.h>
+#include <netinet/in.h>  //
+#include <netinet/tcp.h>
+#include <netdb.h>  //
+#include <memory.h> //
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+typedef int mySocket;
+typedef unsigned int acceptInt;
+#endif
+#ifdef _WIN32
+#include <winsock2.h>
+#include <Mstcpip.h>
+#pragma		comment (lib,"ws2_32.lib")
+typedef SOCKET mySocket;
+typedef int acceptInt;
+#endif
 using namespace std;
 
 char** parcing(char*);
-SOCKET initial();
-SOCKET getConnect(SOCKET);
-void workServer(SOCKET);
-void download(SOCKET, char*);
-void upload(SOCKET);
-void timeSend(SOCKET);
+mySocket initial();
+mySocket getConnect(mySocket);
+void workServer(mySocket);
+void download(mySocket, char*);
+void upload(mySocket);
+void timeSend(mySocket);
+void echo(mySocket, char*);
+void clear();
+void closeMySocket(mySocket);
 
 int main()
 {
-	SOCKET s = initial();
-	while (true){
-		SOCKET s2 = getConnect(s);
-		workServer(s2);
-		closesocket(s2);
+	mySocket s = initial();
+	if (s < 0)
+	{
+		cout << "Error with socket initialization\npress any button to close application." << endl;
+		getchar();
+		return 0;
 	}
-	
-	WSACleanup();
+
+	cout << "server is running" << endl;
+	while (true) {
+		mySocket s2 = getConnect(s);
+		if (s2<0)
+		{
+			cout << "failed connection attempt" << endl;
+			continue;
+		}
+		workServer(s2);
+		closeMySocket(s2);
+		cout << "mySocket closed" << endl;
+	}
+	closeMySocket(s);
+	clear();
 
 	return 0;
+}
+
+void closeMySocket(mySocket s)
+{
+#ifdef WIN32
+	closesocket(s);
+#endif
+#ifdef linux
+	close(s);
+#endif
+}
+
+void clear()
+{
+#ifdef WIN32
+	WSACleanup();
+#endif
 }
 
 char** parcing(char* str)
 {
 	char** response;
 	response = new char*[3];
-	
+
 	int  i = 0;
 	int count = 0;
 	do
 	{
 		char* word = new char[BUFFER];
 		int j = 0;
-		while (str[i] != ' ' && str[i] != '\0')
+		while (str[i] != ' ' && str[i] != '\0' && str[i] != '\n')
 		{
 			word[j] = str[i];
 			i++;
@@ -52,83 +105,170 @@ char** parcing(char* str)
 		response[count] = word;
 		count++;
 		i++;
-	} while (str[i-1] != '\0');
+	} while (str[i - 1] != '\0');
 	return response;
 }
 
-SOCKET initial()
+mySocket initial()
 {
+#ifdef WIN32
 	WORD wVersionRequested;
 	WSADATA wsaData;
 	wVersionRequested = MAKEWORD(2, 2);
 	WSAStartup(wVersionRequested, &wsaData);
-	SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
+#endif
+	mySocket s = socket(AF_INET, SOCK_STREAM, 0);
+	if (s<0)
+	{
+		return -1;
+	}
 	struct sockaddr_in local;
+#ifdef linux
+	memset((char*)&local, '\0', sizeof(local));
+#endif
 	local.sin_family = AF_INET;
 	local.sin_port = htons(1280);
-	DWORD temp = 0;
 
-	WSAGetLastError();
 	local.sin_addr.s_addr = htonl(INADDR_ANY);
 	int c = bind(s, (struct sockaddr*)&local, sizeof(local));
+	if (c < 0)
+	{
+		closeMySocket(s);
+		return -1;
+	}
 	int r = listen(s, 5);
+	if (r < 0)
+	{
+		closeMySocket(s);
+		return -1;
+	}
+
 	return s;
 }
 
-SOCKET getConnect(SOCKET s)
+mySocket getConnect(mySocket s)
 {
 	sockaddr_in remote_addr;
-	int size = sizeof(remote_addr);
-	SOCKET s2 = accept(s, (struct sockaddr*)&remote_addr, &size);
+	acceptInt size = sizeof(remote_addr);
+	mySocket s2 = accept(s, (struct sockaddr*)&remote_addr, &size);
+	if (s2 < 0)
+		return -1;
+#ifdef linux
+	int keepalive = 1;
+	if (setsockopt(s2, SOL_SOCKET, SO_KEEPALIVE, &keepalive, (socklen_t)sizeof(keepalive)) < 0)
+	{
+
+		perror("setsockopt(keepalive)");
+	}
+	int keepcnt = 1;
+	if (setsockopt(s2, IPPROTO_TCP, TCP_KEEPCNT, &keepcnt, (socklen_t)sizeof(int))<0)
+	{
+
+		perror("setsockopt(keepcnt)");
+	}
+	int keepidle = 30;
+	if (setsockopt(s2, IPPROTO_TCP, TCP_KEEPIDLE, &keepidle, (socklen_t)sizeof(int))<0)
+	{
+
+		perror("setsockopt(keepidle)");
+	}
+	int keepintvl = 1;
+	if (setsockopt(s2, IPPROTO_TCP, TCP_KEEPINTVL, &keepintvl, (socklen_t)sizeof(int))<0)
+	{
+		perror("setsockopt(keepalive)");
+	}
+#endif
+#ifdef WIN32
+	tcp_keepalive alive;
+	alive.onoff = 1;
+	alive.keepaliveinterval = 3;
+	alive.keepalivetime = 30000;
+	DWORD ret;
+	DWORD Res = WSAIoctl(s, SIO_KEEPALIVE_VALS, &alive, sizeof(alive), NULL, 0, &ret, NULL, NULL);
+	if (Res == SOCKET_ERROR)
+	{
+		cout << "error with keepalive" << endl;
+	}
+#endif
+	cout << "client " << inet_ntoa(remote_addr.sin_addr) << endl;
 	return s2;
 }
 
-void workServer(SOCKET s)
+void workServer(mySocket s)
 {
 	bool flag = true;
-	char lastCmd[128];
-	lastCmd[0] = '\0';
 	do
 	{
-		byte inbuffer[BUFFER];
 		char command[128];
 		int getSize;
 		getSize = recv(s, command, sizeof(command), 0);
-		
+		if (getSize == -1)
+		{
+			cout << "The mySocket on the client is closed for an unknown reason" << endl;
+			return;
+		}
 		char** com = parcing(command);
 		switch (com[0][0])
 		{
 		case 'u': download(s, com[1]); break;
 		case 'c': flag = false; break;
 		case 't': timeSend(s); break;
-		case 'e': send(s, lastCmd, sizeof(lastCmd), 0); break;
+		case 'e': echo(s, com[1]); break;
 		default: break;
 		}
-		strcpy(lastCmd, command);
 	} while (flag);
+	cout << "client closed mySocket" << endl;
 }
 
-void download(SOCKET s, char* path)
+void download(mySocket s, char* path)
 {
 	int getSize;
-	FILE* file = fopen(path, "wb");
-	byte* b = new byte[BUFFER];
-	while (1)
+	int sizeFile;
+	if (recv(s, (char*)(&sizeFile), sizeof(sizeFile), 0)<0)
 	{
-		getSize = recv(s, (char*)b, BUFFER, 0);
-		fwrite(b, sizeof(byte), getSize, file);
-		if (getSize != BUFFER) break;
+		cout << "Error retrieving data" << endl;
+		return;
 	}
+	if (sizeFile == -1)
+	{
+		return;
+	}
+	FILE* file = fopen(path, "wb");
+	cout << "Download file: " << path << endl;
+	char* b = new char[BUFFER];
+	int sizeDownload = 0;
+	while (sizeDownload < sizeFile)
+	{
+
+		getSize = recv(s, b, BUFFER, 0);
+		if (getSize < 0)
+		{
+			cout << "Error retrieving data" << endl;
+			return;
+		}
+		sizeDownload += getSize;
+		fwrite(b, sizeof(char), getSize, file);
+	}
+	cout << "Download finish" << endl;
 	fclose(file);
 }
 
-void timeSend(SOCKET s)
+void timeSend(mySocket s)
 {
 	char buffer[10];
 	time_t seconds = time(NULL);
 	tm* timeinfo = localtime(&seconds);
 	char* format = "%H:%M:%S";
 	strftime(buffer, 10, format, timeinfo);
-	cout << "Current Datetime: " << buffer << endl;
 	send(s, buffer, sizeof(buffer), 0);
+	cout << "Time sended on client" << endl;
+}
+
+void echo(mySocket s, char* str)
+{
+	if (send(s, str, strlen(str) + 1, 0)<0)
+	{
+		cout << "Error sending data" << endl;
+		return;
+	}
 }
